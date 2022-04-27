c35c1d4fd9413ac061c6b0d2839d2e7691447c43
nelhalla <newinfos55@yahoo.fr>
2022-04-27 12:17:09 +0000

    Ajout de l annotation CrossOrigin + maj init.sql

--- src/main/java/com/cofomo/product/microservice/web/controller/ProductController.java
+++ src/main/java/com/cofomo/product/microservice/web/controller/ProductController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController implements ProductApi {
 
     ProductService productService;
